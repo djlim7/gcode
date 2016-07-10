902da4c186c4df2c561fd90b104d807029054e0c
Dong-jun Lim <ciqete@gmail.com>
2016-07-10 13:53:37 +0900

    Put gcode_object

--- gcode.py
+++ gcode.py
@@ -1,7 +1,9 @@
 import argparse
+import gcode_object
 
-parser = argparse.ArgumentParser()
-parser.add_argument('input_file', nargs = '?')
+parser_obj = argparse.ArgumentParser()
+parser_obj.add_argument('input_file', nargs = '?')
+parser_args = parser_obj.parse_args()
 
 if __name__ == "__main__":
-	pass
+	print(parser_args.input_file)
---
+++ gcode_object.py
@@ -0,0 +1,6 @@
+class gcode_info:
+	field = 'None'
+	integar = 'None'
+	def __init__(self, f, i):
+		field = f
+		integar = i
