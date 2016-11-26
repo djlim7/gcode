c6eadf9c312e4950e973d2f7cba9442bdb4b91d5
Dong-jun Lim <ciqete@gmail.com>
2016-11-26 17:29:10 +0900

    Add lexical_parser

--- GCodeProcedure.py
+++ GCodeProcedure.py
@@ -14,13 +14,13 @@ class GCodeParser:
     def __init__(self, process_string):
         self.process_string = process_string
 
-    def parse_syntax(self):
-        """Parse the syntax, form text file to Python tuple."""
+    def lexical_parse(self):
+        """Lexical parse, form text file to Python tuple."""
         main_loop = True
         idx = 0
         result_list = []
         last_processed_type = 'space'
-        # 'str', 'int', 'space', 'minus', 'dot', '%'
+        # 'char', 'int', 'space', 'minus', '.', '%', '(', ')'
         while main_loop:
             # Check EOF
             if idx == len(self.process_string):
@@ -29,22 +29,25 @@ class GCodeParser:
             else:
                 character = self.process_string[idx]
 
-            # 'str'
+            # 'char'
             if character in string.ascii_letters:
                 result_list.append(character.upper())
-                last_processed_type = 'str'
+                last_processed_type = 'char'
             # 'int'
             elif character.isdigit():
-                if last_processed_type == 'str':
-                    result_list.append(float(character))
-                elif last_processed_type == 'int':
+                if last_processed_type == 'int':
                     result_list[-1] = float(result_list[-1]) * 10 + float(character)
-                elif last_processed_type == 'space':
+                else:
                     result_list.append(float(character))
                 last_processed_type = 'int'
             # 'space'
             elif character.isspace():
                 last_processed_type = 'space'
+            # '.', '%', '(', ')'
+            elif character == '.' or character == '%' or \
+                character == '(' or character == ')':
+                result_list.append(character)
+                last_processed_type = str(character)
             else:
                 raise GCodeObject.GCodeSyntaxError \
                     ('The file contains unsupported character.')
